162432ca7c949de94b58887cb2b29b8e04872da9
HolmanCanyon <canyonholman@u.boisestate.edu>
2026-03-03 09:41:43 -0700

    Add top module with half subtractor instantiation

---
+++ top.v
@@ -0,0 +1,14 @@
+module top(
+  input [1:0]sw,
+  output [1:0]led
+);
+    
+    half_sub half_sub(
+      .A(sw[0]),
+      .B(sw[1]),
+      .Y(led[0]),
+      .Carry(led[1])
+        
+    );
+  
+endmodule
